ae6c2da38779c4e8ce35c3662bcf85336ee8d6d5
F1ST <Konrad03Nowak@gmail.com>
2026-02-26 22:04:46 +0100

    this file replaces the older one, so the naming is more correct

---
+++ src/implementations.cpp
@@ -0,0 +1,7 @@
+// Compile STB implementation in its own translation unit so third-party
+// warnings don't pollute the main build.  This file is compiled with -w.
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
